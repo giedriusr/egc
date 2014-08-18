1c412b844763620ca8fe26705c1c9faf070de3e8
Giedrius Rimkus <info@giedriusr.lt>
2014-08-18 13:23:09 +0300

    Add failing RSpec with Gadget relations

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,6 @@
 FactoryGirl.define do
   factory :user do
+    email 'test@email.com'
+    password 123456789
   end
 end
--- spec/models/gadget_spec.rb
+++ spec/models/gadget_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Gadget, type: :model do
+  describe 'relations' do
+    it 'belongs to a user' do
+      gadget = create(:gadget, user: create(:user))
+      expect(gadget.user.email).to eq('test@email.com')
+    end
+  end
 end
