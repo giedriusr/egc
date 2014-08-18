6c8ede0f35344a518d9974afaf0a33f104637d44
Giedrius Rimkus <info@giedriusr.lt>
2014-08-18 13:25:59 +0300

    Add failing spec for user relations.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'relations' do
+    it 'has many gadgets' do
+      user = create(:user)
+      create_list(:gadget, 5, user: user)
+      expect(user.gadgets.size).to eq(5)
+    end
+  end
 end
