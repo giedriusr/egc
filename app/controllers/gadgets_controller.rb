2e9cc58fb7a27708aa6ab01f038948cc2e11d22b
Giedrius Rimkus <info@giedriusr.lt>
2014-08-18 14:17:59 +0300

    Add create new gadget implementation and pass failing specs

--- app/controllers/gadgets_controller.rb
+++ app/controllers/gadgets_controller.rb
@@ -7,10 +7,27 @@ class GadgetsController < ApplicationController
     @gadget = Gadget.new
   end
 
+  def create
+    @gadget = Gadget.new
+
+    if @gadget.update_attributes(gadget_params)
+      flash[:success] = 'Saved'
+      redirect_to gadgets_path
+    else
+      render :new
+    end
+  end
+
   def edit
     @gadget = Gadget.find(params[:id])
   end
 
   def show
+  end
+
+  private
+
+  def gadget_params
+    params.require(:gadget).permit(:name, :description)
   end
 end
