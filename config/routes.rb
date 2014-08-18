938aa0dff597e86371990a6838cb73288b933a94
Giedrius Rimkus <info@giedriusr.lt>
2014-08-18 12:58:15 +0300

    Make shorter devise urls

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   devise_for :users
+  devise_scope :user do
+    delete '/sign_out', to: 'devise/sessions#destroy', as: 'sign_out'
+    get '/sign_in', to: 'devise/sessions#new', as: 'sign_in'
+    get '/users/sign_in', to: 'devise/sessions#new', as: 'users_sign_in'
+    get '/sign_up', to: 'devise/registrations#new', as: 'sign_up'
+    get '/reset_password', to: 'devise/passwords#new', as: 'reset_password'
+  end
 end
